d0e124562361c4efe083794a92268968115af53a
carlromeo <carlromeo.dogbey@gmail.com>
2019-09-04 18:05:55 -0400

    math_two

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import static java.lang.Integer.MAX_VALUE;
+
+public class Main {
+    public static void main (String[]args){
+        int x, y, answer;
+        double myothernumber;
+        x = 99;
+        y = MAX_VALUE;
+        answer= 2;
+        myothernumber = 1.7938;
+
+        System.out.println("My number is" + " " + answer);
+        System.out.println("My other number is" + " "+ myothernumber);
+
+
+    }
+}
